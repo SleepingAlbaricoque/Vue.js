b48666970db030b188bd3fee33971766c6a14faf
SleepingAlbaricoque <tnqls0421@naver.com>
2023-04-13 12:47:34 +0900

    2023.04.13 JWT 예제

--- Ch07/src/routers/index.js
+++ Ch07/src/routers/index.js
@@ -6,13 +6,24 @@ import { createRouter, createWebHistory } from "vue-router";
 import AxiosComponent from "../components/sub1/AxiosComponent.vue";
 import RestComponent from "../components/sub2/RestComponent.vue";
 import JWTComponent from "../components/sub3/JWTComponent.vue";
+import LoginForm from "../components/sub3/LoginForm.vue";
+import LoginSuccess from "../components/sub3/LoginSuccess.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/axios", name: "AxiosComponent", component: AxiosComponent },
     { path: "/rest", name: "RestComponent", component: RestComponent },
-    { path: "/jwt", name: "JWTComponent", component: JWTComponent },
+    {
+      path: "/jwt",
+      name: "JWTComponent",
+      component: JWTComponent,
+      children: [
+        { path: "", component: LoginForm }, //JWTComponent 화면에서 호출하면 LoginForm 컴포넌트 내용이 뜨는 것
+        { path: "loginForm", component: LoginForm },
+        { path: "loginSuccess", component: LoginSuccess },
+      ],
+    },
   ],
 });
 
